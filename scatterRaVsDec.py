6aba82c8f94bfba22f2022fbdc3b43b21f00c5d7
Mq1t <matthewmcg815@gmail.com>
2026-06-01 14:38:27 -0400

    Create scatterRaVsDec.py
    Made a small function for a scatter plot of Ra vs Dec

---
+++ scatterRaVsDec.py
@@ -0,0 +1,12 @@
+    #RA, X-Value
+    x = df['ra']
+    #Declination, Y-Values
+    y = df['dec']
+    
+    plt.style.use('dark_background')
+    plt.scatter(x, y, s = 0.2)
+    plt.title(name+": RA vs Declination")
+    plt.xlabel("RA")
+    plt.ylabel("Declination")
+    plt.show()
+
