07bdefc136a74f1dd097d56255ff7f31826d7923
Mq1t <matthewmcg815@gmail.com>
2026-06-08 15:34:49 -0400

    Update documentation

--- gaiadr3_analysis/scatterplot.py
+++ gaiadr3_analysis/scatterplot.py
@@ -5,6 +5,24 @@ import pandas as pd
 
 #Maybe change name to scatter_coordinates? or scatter_location?
 def ra_dec(df: pd.DataFrame, xlim: int|float = None, ylim: int|float = None, color: str ='red', size: int|float = 0.5, title: str = 'Right Ascension Vs. Declination'):
+    """
+    Plot Right Ascension (RA) vs Declination (Dec) from a pandas DataFrame.
+
+    Parameters:
+        df (pd.DataFrame): A pandas DataFrame containing at least two columns, 'ra' for Right Ascension
+                          and 'dec' for Declination.
+        xlim (int|float, optional): The x-axis limits as a tuple (xmin, xmax). If None, the default limits are used. Default is None.
+        ylim (int|float, optional): The y-axis limits as a tuple (ymin, ymax). If None, the default limits are used. Default is None.
+        color (str, optional): Color of the plotted points. Default is 'red'.
+        size (int|float, optional): Size of the plotted points. Default is 0.5.
+        title (str, optional): Title of the plot. Default is 'Right Ascension Vs. Declination'.
+
+    Raises:
+        TypeError: If the input data is not a pandas DataFrame.
+
+    Returns:
+        None
+    """
     if not isinstance(df, pd.DataFrame):
         raise TypeError('Data must be of type pandas.DataFrame')
     
@@ -25,7 +43,24 @@ def ra_dec(df: pd.DataFrame, xlim: int|float = None, ylim: int|float = None, col
 
 #Proper motion
 def proper_motion(df: pd.DataFrame, xlim:float=None, ylim=None, color: str ='red', size: int|float = 0.5, title: str = 'Right Ascension Vs. Declination in Proper Motion Space'):
-    #Checks if data is of type pandas dataframe.
+    """
+    Plot Right Ascension (RA) vs Declination (Dec) in proper motion space from a pandas DataFrame.
+
+    Parameters:
+        df (pd.DataFrame): A pandas DataFrame containing at least two columns, 'pmra' for Proper Motion in RA
+                          and 'pmdec' for Proper Motion in Dec.
+        xlim (float, optional): The x-axis limits as a tuple (xmin, xmax). If None, the default limits are used. Default is None.
+        ylim (float, optional): The y-axis limits as a tuple (ymin, ymax). If None, the default limits are used. Default is None.
+        color (str, optional): Color of the plotted points. Default is 'red'.
+        size (int|float, optional): Size of the plotted points. Default is 0.5.
+        title (str, optional): Title of the plot. Default is 'Right Ascension Vs. Declination in Proper Motion Space'.
+
+    Raises:
+        TypeError: If the input data is not a pandas DataFrame.
+
+    Returns:
+        None
+    """
     if not isinstance(df, pd.DataFrame):
         raise TypeError('Data must be of type pandas.DataFrame')
     
